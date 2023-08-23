d1229ea7a38f699a21fbdc28c166f2c095ea3aba
MaksymBora <maxboraod@gmail.com>
2023-08-23 23:19:38 +0200

    add common styles

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -8,20 +8,29 @@ body {
   -moz-osx-font-smoothing: grayscale;
 	margin: 0;
   padding: 10px;
-	color: #000;
-  }
+	color: rgb(255, 255, 255);
+  background-color: rgb(35, 39, 47);
+
+  -webkit-scrollbar-track {
+    display: none;
+  };
+
+  -webkit-scrollbar {
+    display: none;
+  };
+  };
 
 code {
     font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
       monospace;
-  }
+  };
 
 ul {
     list-style: none;
     padding-left: 0;
     margin-top: 0;
     margin-bottom: 0;
-}
+};
 
   h1,
   h2,
@@ -31,13 +40,12 @@ ul {
   h6,
   p {
     margin: 0;
-}
+};
 
 img {
     display: block;
     max-width: 100%;
     height: auto;
-}
-
+};
 
 `;
