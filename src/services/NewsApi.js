49f5e58e0fff059703fba9018ce9205174d7a268
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-08-23 20:34:02 -0500

    Update NewsApi.js

--- src/services/NewsApi.js
+++ src/services/NewsApi.js
@@ -1,13 +1,30 @@
 import axios from 'axios';
 
-const BASIC_URL = process.env.REACT_APP_API_URL;
-const API_KEY = process.env.REACT_APP_API_KEY;
+const BASIC_URL = 'https://newsapi.org/v2/everything';
+const API_KEY = '15a02b4161eb4df2bc84f6211734c445';
 
 export async function getNews() {
   try {
-    const response = await axios.get(`${BASIC_URL}?access_key=${API_KEY}`);
+    const keywords = [
+      'IT',
+      'Ukraine',
+      'technology',
+      'car',
+      'sport',
+      'business',
+      'science',
+    ];
+    const queryString = keywords.join(' OR ');
+
+    const response = await axios.get(BASIC_URL, {
+      params: {
+        apiKey: API_KEY,
+        q: queryString,
+      },
+    });
     console.log(response);
   } catch (error) {
-    console.error('Error:', error.response);
+    console.error('Error:', error.response.data.message);
+    throw error;
   }
 }
