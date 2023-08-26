3aa18ba0e8579756e2f5053b63a7da7ea06bd6ce
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-08-26 13:48:13 -0500

    update

--- src/components/Main/MyVideos/VideoCard/VideoCard.js
+++ src/components/Main/MyVideos/VideoCard/VideoCard.js
@@ -1,14 +1,15 @@
 import genres from '../../../../data/genres.json';
 
+import {
+  StyledMovieCardWrapper,
+  StyledMovieTitle,
+  StyledMovieImg,
+  StyledMovieOverview,
+  StyledMovieGenre,
+} from './VideoCard.styled';
+
 export const VideoCard = ({
-  movies: {
-    title,
-    poster_path,
-    genre_ids,
-    overview,
-    release_date,
-    vote_average,
-  },
+  movies: { title, poster_path, genre_ids, overview, release_date },
 }) => {
   const getMovieGenres = function (genre_ids) {
     return genre_ids.map(id => {
@@ -20,18 +21,17 @@ export const VideoCard = ({
   const genresNames = getMovieGenres(genre_ids);
 
   return (
-    <li>
-      <div>
-        <p>{title}</p>
-        <p>Genre: {genresNames.join(', ')}</p>
-        <p>vote average: {vote_average}</p>
-        <p>{overview}</p>
-        <p>Release date: {release_date}</p>
-        <img
-          src={`https://image.tmdb.org/t/p/w500${poster_path}`}
-          alt={title}
-        />
-      </div>
-    </li>
+    <StyledMovieCardWrapper>
+      <StyledMovieTitle>{title}</StyledMovieTitle>
+
+      <StyledMovieOverview>{overview}</StyledMovieOverview>
+      <StyledMovieImg
+        src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+        alt={title}
+      />
+      <StyledMovieGenre>Genre: {genresNames.join(', ')}</StyledMovieGenre>
+
+      <p>Release date: {release_date}</p>
+    </StyledMovieCardWrapper>
   );
 };
---
+++ src/components/Main/MyVideos/VideoCard/VideoCard.styled.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const StyledMovieCardWrapper = styled.li`
+  border-top: 1px solid grey;
+  border-bottom: 1px solid grey;
+  border-radius: 10px;
+  margin-bottom: 20px;
+  padding: 20px;
+`;
+
+export const StyledMovieTitle = styled.p`
+  font-size: 22px;
+  font-weight: 600;
+  margin-bottom: 12px;
+`;
+
+export const StyledMovieImg = styled.img`
+  margin-bottom: 12px;
+`;
+
+export const StyledMovieOverview = styled.p`
+  font-size: 16px;
+  margin-bottom: 12px;
+`;
+
+export const StyledMovieGenre = styled.p`
+  margin-bottom: 12px;
+`;
