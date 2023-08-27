f4a5bebe6f2f1c5ef6e5c92a0ed1268afa526589
MaksymBora <maxboraod@gmail.com>
2023-08-27 14:46:24 +0200

    add icons

--- src/components/Header/WeatherCard/WeatherCard.jsx
+++ src/components/Header/WeatherCard/WeatherCard.jsx
@@ -1,4 +1,31 @@
-export const WeatherCard = ({items : {city,
+import { CiTempHigh } from "react-icons/ci";
+import { FaTemperatureArrowUp, FaTemperatureArrowDown, FaCloud, FaCloudShowersHeavy, FaCloudRain, FaSun, FaCloudSun } from "react-icons/fa6";
+import {WeatherWrapper} from './WeatherCard.style'
+
+function weatherIcon (weather) {
+	switch (weather) {
+		case 'Clouds':
+			return <FaCloud />;
+			
+		case 'Moderate rain':
+			return <FaCloudShowersHeavy />;
+			
+		case 'Light rain':
+			return <FaCloudRain />;
+			
+		case 'Clear sky':
+			return <FaSun />;
+			
+		case 'Broken clouds':
+			return <FaCloudSun />;
+			
+		default:
+			return <FaSun />;
+			
+	}
+};
+
+export const WeatherCard = ({ items: { city,
     temperature,
     tempFeelsLike,
     tempMax,
@@ -6,13 +33,14 @@ export const WeatherCard = ({items : {city,
     weatherState}}) => {
 	return (
 		<>
-			<div>
-				<h2>{city}</h2>
-				<p>Current temperature: {temperature.toFixed(0)}°C</p>
+			<WeatherWrapper>
+				<h2>{ city }</h2>
+				<CiTempHigh size={24}/>
+				<p> {temperature.toFixed(0)}°C</p>
 				<p>Feels like: {tempFeelsLike.toFixed(0)}°C</p>
-				<p>Max: {tempMax.toFixed(0)}°C | Min: {tempMin.toFixed(0)}°C</p>
-				<p>{weatherState}</p>
-			</div>
+				<p><FaTemperatureArrowUp size={24}/> {tempMax.toFixed(0)}°C | <FaTemperatureArrowDown size={24}/> {tempMin.toFixed(0)}°C</p>
+				<p>{weatherIcon(weatherState)}</p>
+			</WeatherWrapper>
 		</>
 	)
 };
---
+++ src/components/Header/WeatherCard/WeatherCard.style.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const WeatherWrapper = styled.div`
+  display: flex;
+`;
