7a3d3ffe3e994713337489dab563063dfc717f29
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-09-07 23:20:09 -0500

    Update Sidebar.styled.js

--- src/components/Sidebar/Sidebar.styled.js
+++ src/components/Sidebar/Sidebar.styled.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export const StyledSidebarWrapper = styled.div`
   border-right: 1px solid orange;
   border-radius: 12px;
-  width: 250px;
+  width: 200px;
   height: 100vh;
 `;
 
